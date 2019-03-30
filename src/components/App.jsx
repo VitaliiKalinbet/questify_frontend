85404d4db4e4c32d49c7460baec17a6ac2904c6c
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-03-30 14:12:18 +0200

    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import DashboardPage from '../pages/DashboardPage/DashboardPage';
+import Header from './Header/Header';
 
 class App extends Component {
   state = {};
@@ -11,8 +12,12 @@ class App extends Component {
     return (
       <div>
         <Router>
-          <Route exact path="/" component={LoginPage} />
-          <Route path="/dashboard" component={DashboardPage} />
+          <Header />
+
+          <Switch>
+            <Route exact path="/" component={LoginPage} />
+            <Route path="/dashboard" component={DashboardPage} />
+          </Switch>
         </Router>
       </div>
     );
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -4,11 +4,7 @@ class DashboardPage extends Component {
   state = {};
 
   render() {
-    return (
-      <div>
-        <h2>DashboardPage</h2>
-      </div>
-    );
+    return <div>DashboardPage</div>;
   }
 }
 
