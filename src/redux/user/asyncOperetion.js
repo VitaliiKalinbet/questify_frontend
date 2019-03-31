7b0a9fc42e02f3101b4ea384c29d9cb3453b37a4
vvp <vovvva33@gmail.com>
2019-03-31 12:31:51 +0300

    redux is working

--- src/redux/user/asyncOperetion.js
+++ src/redux/user/asyncOperetion.js
@@ -1,12 +1,12 @@
-import { send } from '../../utils/axiosOperetions';
+import { userLogin } from '../../utils/axiosOperetions';
 
 import { Err, Request, Success } from './userAction';
 import { success } from '../auth/authAction';
 
 const loginUser = user => dispatch => {
   dispatch(Request());
-  return send(user)
-    .then(response => dispatch(Success(response)))
+  return userLogin(user)
+    .then(response => dispatch(Success(response.data.data)))
     .then(() => dispatch(success()))
     .catch(error => dispatch(Err(error)));
 };
