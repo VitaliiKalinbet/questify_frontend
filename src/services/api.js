80b4a5620836bc613d407318598c0948007eaa4a
Viktor Bulvarenko <silken.ua@gmail.com>
2019-05-07 16:59:20 +0300

    fix api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://questify.vbguard.dev/api';
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = 'https://questify.vbguard.dev/api';
+// axios.defaults.baseURL = 'http://localhost:5000/api';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const fetchUser = userNickname => axios.post('/login', userNickname);
